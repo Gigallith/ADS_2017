37b5a9076f3470196c82811b0c532c40b44390a3
Gigallith <paul.dupont@etu.unice.fr>
2017-08-21 08:46:01 +0200

    Initial commit

---
+++ main.h
@@ -0,0 +1,24 @@
+//
+// Created by user on 20/08/2017.
+//
+
+#ifndef ADS_2017_MAIN_H
+#define ADS_2017_MAIN_H
+
+#endif //ADS_2017_MAIN_H
+
+
+
+struct Edge
+{
+    int x;
+    int y;
+};
+
+struct WeightedUnDiGraph
+{
+    struct Edge;
+    int variable2;
+    int autreVariable;
+    double nombreDecimal;
+};
